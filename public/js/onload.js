62c19dccab0d7f1b8b5ac9d698ed8a5bdd2547a7
Eirik Brandtzæg <eirikb@eirikb.no>
2011-07-04 00:14:44 +0200

    ONE COMMIT TO RULE THEM ALL!

---
+++ public/js/onload.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('table').dataTable({
+        aaSorting: [[3, 'desc']],
+        bLengthChange: false,
+        bInfo: false,
+        sPaginationType: 'full_numbers',
+        iDisplayLength: 30
+    });
+});
+
