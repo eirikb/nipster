39d6dd1dcab8f2cd170caadcd3c8beec3219119c
Eirik Brandtzæg <eirikb@eirikb.no>
2011-07-04 00:14:44 +0200

    ONE COMMIT TO RULE THEM ALL!

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+path = require('path'),
+rated = require('rated.js'),
+app = module.exports = express.createServer();
+
+app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname + '/public')));
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Nipster!',
+        packages: rated.packages
+    });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
