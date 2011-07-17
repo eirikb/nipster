627144c8d816cdd75d971e8858f6798b42cef159
Eirik Brandtzæg <eirikb@eirikb.no>
2011-07-17 11:53:12 +0200

    Toggle searching untracked packages

--- static/js/onload.js
+++ static/js/onload.js
@@ -1,12 +1,35 @@
 $(function() {
-    $('table').dataTable({
-        aaSorting: [[3, 'desc']],
-        bLengthChange: false,
-        bInfo: false,
-        sPaginationType: 'full_numbers',
-        iDisplayLength: 30,
-        bProcessing: true,
-        sAjaxSource: 'json/packages.json'
-    });
+	$.get('json/packages.json', function(data) {
+		console.log(data);
+		var i, l, $includeUntracked, $table;
+		for (i = 0; i < data.packages.length; i++) {
+			l = data.packages[i];
+			l[0] = '<a href="' + data.urls[i] + '">' + l[0] + '</a>';
+		}
+		$table = $('table').dataTable({
+			aaSorting: [[3, 'desc']],
+			bLengthChange: false,
+			bInfo: false,
+			sPaginationType: 'full_numbers',
+			iDisplayLength: 15,
+			bProcessing: true,
+			aaData: data.packages
+		});
+
+		$includeUntracked = $('<div>').append($('<input id="includeUntracked" type="checkbox">').change(function() {
+            var i, untracked;
+			$table.fnClearTable();
+			$table.fnAddData(data.packages);
+			if (this.checked) {
+                untracked = [];
+				for (i = 0; i < data.untracked.length; i++) {
+					untracked.push([data.untracked[i][0], '', '', '']);
+				}
+                $table.fnAddData(untracked);
+			}
+		})).append('<label for="includeUntracked">Include untracked');
+
+		$('div.dataTables_filter').append($includeUntracked);
+	});
 });
 
